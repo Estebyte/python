38319459e882a21bb53f8e843e35e78fc4569a44
Esteban <103667018+Estebyte@users.noreply.github.com>
2024-08-07 13:15:24 +0100

    Delete Fundamentos Python directory

--- Fundamentos Python/Teorema Pitagoras.py
+++
@@ -1,8 +0,0 @@
-cateto_a = int(input("Cateto a:"))
-cateto_b = int(input("Cateto b:"))
-
-sumadeloscatetos = (cateto_a**2)+(cateto_b**2)
-
-hipotenusa = f"la hipotenusa es: {sumadeloscatetos**0.5}"
-print (hipotenusa)
-
