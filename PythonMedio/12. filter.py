3689bc3010fbe81b473c18c1200aaded07ab96eb
Esteban <103667018+Estebyte@users.noreply.github.com>
2024-08-07 13:15:03 +0100

    Delete PythonMedio directory

--- PythonMedio/12. filter.py
+++
@@ -1,4 +0,0 @@
-numbers = [1, 4, 3, 8]
-even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
-print(numbers)
-print(even_numbers)
--- PythonMedio/4. list_comprenhension.py
+++
@@ -1,6 +0,0 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = [x for x in fruits if "a" in x]
-print(newlist)
-
-numbers = [i for i in range(1,11) if i % 2 == 0]
-print(numbers)
